768f63dc915989cd6d762443d79bdc776ecda543
j <dirkmcpherson@gmail.com>
2025-03-27 19:19:41 -0400

    force control on gripper rather than position control. More parameterization of example script

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 import argparse
 import genesis as gs
 import time
+from collections import defaultdict
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--vis", action="store_true", default=False)
@@ -8,6 +9,9 @@ parser.add_argument("-d", "--debug", action="store_true", default=False)
 parser.add_argument("-p", "--position", type=int, default=-1)
 parser.add_argument("-r", "--radius", type=float, default=0.03)
 parser.add_argument("-f", "--friction", type=float, default=0.5)
+parser.add_argument("-e", "--height", type=float, default=0.085)
+parser.add_argument("-o", "--rho", type=float, default=2000)
+
 args = parser.parse_args()
 
 assert args.position in [-1, 0, 1, 2], "Position must be -1, 0, 1, or 2"
@@ -23,8 +27,8 @@ plane = scene.add_entity(
     gs.morphs.Plane(),
 )
 
-BOTTLE_RADIUS = 0.03
-BOTTLE_HEIGHT = 0.08
+BOTTLE_RADIUS = args.radius
+BOTTLE_HEIGHT = args.height
 BOX_WIDTH, BOX_HEIGHT = 0.75, 0.12
 
 box = scene.add_entity(
@@ -37,11 +41,12 @@ box = scene.add_entity(
 )
 
 import pathlib as pl
+# TODO: see if you can prevent the gripper from being convexified
 kinova = scene.add_entity(
     gs.morphs.URDF(
         file=str(pl.Path(__file__).parent / 'gen3_lite_2f_robotiq_85.urdf'),
         fixed=True,
-        convexify=False,
+        convexify=True,
         pos=(0.0, 0.0, 0.05), # raise to account for table mount
     ),
     vis_mode="collision"
@@ -50,29 +55,32 @@ kinova = scene.add_entity(
 )
 
 STATIC_BOTTLE_POSITION = (0.6, -0.2, 0.19)
-POSITION_0 = (0.4381, 0.1, 0.05)
-POSITION_1 = (0.4381, -0.05, 0.05)
-POSITION_2 = (0.4381, -0.2, 0.05)
+PX, PZ = 0.475, 0.05
+POSITION_0 = (PX, 0.1, PZ)
+POSITION_1 = (PX, -0.05, PZ)
+POSITION_2 = (PX, -0.2, PZ)
 
 
-
+# TODO: make the bottle slightly deformable
 bottle = scene.add_entity(
-    material=gs.materials.Rigid(rho=3000,
+    material=gs.materials.Rigid(rho=args.rho,
                                 friction=args.friction),
+    # material=gs.materials.Rigid(rho=args.rho,
+    #                             friction=args.friction),
     morph=gs.morphs.Cylinder(
         pos=POSITION_0,
-        radius=args.radius,
+        radius=BOTTLE_RADIUS,
         height=BOTTLE_HEIGHT,
     ),
     # visualize_contact=True,
 )
 
 goal_bottle = scene.add_entity(
-    material=gs.materials.Rigid(rho=1000,
+    material=gs.materials.Rigid(rho=args.rho,
                             friction=2.0),
     morph=gs.morphs.Cylinder(
         pos=STATIC_BOTTLE_POSITION,
-        radius=args.radius,
+        radius=BOTTLE_RADIUS,
         height=BOTTLE_HEIGHT,
     ),
     # visualize_contact=True,
@@ -109,7 +117,7 @@ print(kinova.get_dofs_force_range(dofs_idx_local=kdofs_idx))
 # )
 
 print(f"Hard setting kinova joint positions")
-harcoded_start = [0.3268500269015339, -1.4471734542578538, 2.3453266624159497, -1.3502152158191212, 2.209384006676201, -1.5125125137062945, -1, 1, -0.5 ,0.5]
+harcoded_start = [0.3268500269015339, -1.4471734542578538, 2.3453266624159497, -1.3502152158191212, 2.209384006676201, -1.5125125137062945, -1, 1, 0.6 , 0.6]
 kinova.set_dofs_position(np.array(harcoded_start), kdofs_idx)
 
 
@@ -178,13 +186,15 @@ for path in dir.iterdir():
     if not setup(): continue
     trials_ran += 1
 
-    time.sleep(5)
-
-    import cv2
+    # time.sleep(1)
 
     QUIT = False
-    step = cmd_idx = reward = 0
+    step = reward = 0
+    cmd_idx = 5 # avoid the initial position
+    cmd_idx_count = defaultdict(int)
+    pos = kinova.get_dofs_position(dofs_idx_local=kdofs_idx)
     while cmd_idx < len(vel_cmd) and not QUIT:
+        cmd_idx_count[cmd_idx] += 1
         # QUIT = not listener.running
         if cmd_idx >= len(vel_cmd):
             cmd = np.zeros(6)
@@ -192,7 +202,7 @@ for path in dir.iterdir():
             setup()
         else:
             cmd = vel_cmd[cmd_idx]
-            cmd_idx += 1 if not args.debug else 2
+            cmd_idx += 1
         # cmd[0] = +1.0
 
         # kinova.control_dofs_velocity(cmd, dofs_idx_local=kdofs_idx[:len(cmd)])
@@ -203,11 +213,36 @@ for path in dir.iterdir():
         # print the distance from the current position to the commanded position
 
         kinova.control_dofs_position(cmd, dofs_idx_local=kdofs_idx[:len(cmd)])
-        if cmd_idx < len(gripper_pos):
+        if cmd_idx < len(gripper_pos): # TODO: Don't just close the gripper, control with force control. Make is to the motor command stops when you get a high enough force back on the joint.
             # map from 0, 100 to -1, 0 for the left finger and 0, 100 to 0, 1 for the right finger
+            # motor_cmd = (100 - gripper_pos[cmd_idx][0]) / 100
+            # gripper_cmd = [-motor_cmd, motor_cmd, -0.5, -0.5]
+            # kinova.control_dofs_position(gripper_cmd, dofs_idx_local=np.array(kdofs_idx[-4:]))
+            
+            output_force = [0., 0., 0., 0.]
             motor_cmd = (100 - gripper_pos[cmd_idx][0]) / 100
-            gripper_cmd = [-motor_cmd, motor_cmd, -0.5, -0.5]
-            kinova.control_dofs_position(gripper_cmd, dofs_idx_local=np.array(kdofs_idx[-4:]))
+
+            right_error = pos[-4] + motor_cmd; right_error = right_error if abs(right_error) > 0.05 else 0.0
+            left_error = pos[-3] - motor_cmd; left_error = left_error if abs(left_error) > 0.05 else 0.0
+            right_fingertip_error = pos[-2] - harcoded_start[-2]; right_fingertip_error = right_fingertip_error if abs(right_fingertip_error) > 0.05 else 0.0
+            left_fingertip_error = pos[-1] - harcoded_start[-1]; left_fingertip_error = left_fingertip_error if abs(left_fingertip_error) > 0.05 else 0.0
+
+            output_force[0] = -2*right_error; output_force[2] = 1*right_fingertip_error
+            output_force[1] = -2*left_error; output_force[3] = 1*left_fingertip_error
+
+            # print([f'{entry:1.1f}' for entry in output_force], f'{right_error:+1.2f}', f'{left_error:+1.2f}')
+            kinova.control_dofs_force(output_force, dofs_idx_local=np.array(kdofs_idx[-4:]))
+
+        # def apply_force_cmd(f):
+        #     # apply a force to the end effector
+        #     # kinova.control_dofs_force(f, dofs_idx_local=kdofs_idx[:len(cmd)])
+        #     kinova.control_dofs_force(f, dofs_idx_local=np.array(kdofs_idx[-4:]))
+
+        #     for _ in range(30):
+        #         scene.step()
+        # from IPython import embed; embed();
+
+
         # print('motor ', ', '.join([f'{c:+1.2f}' for c in cmd]), end=' ')
         # print('gripper ', ', '.join([f'{c:+1.2f}' for c in gripper_cmd]))
 
@@ -227,9 +262,20 @@ for path in dir.iterdir():
         pos = kinova.get_dofs_position(dofs_idx_local=kdofs_idx)
         total_diff = sum([abs(jp - cjp) for jp, cjp in zip(pos, cmd)])
 
-        if total_diff > 0.01: 
-            # print(f"Failed to reach commanded position at step {step} with diff {total_diff}. repeating command")
-            cmd_idx -= 1
+        if total_diff > 0.15:
+            if cmd_idx_count[cmd_idx] > 50:
+                print(f"Failed to reach commanded position at step {step} with diff {total_diff}. Next command after {cmd_idx_count[cmd_idx]} steps.")
+            else:
+                if args.debug:
+                    print(f"Failed to reach commanded position at step {step} with diff {total_diff}. repeating command")
+                    for jp, cjp in zip(pos, cmd):
+                        print(f"\tJoint position: {jp:1.2f}, Commanded position: {cjp:1.2f}")
+                cmd_idx -= 1
+        else:
+            # print(f"Reached commanded position at step {step} with diff {total_diff}")
+            if args.debug:
+                print("="*10)
+            continue
 
         if step % reward_check_period == 0:
             # bottles need to be in contact
@@ -240,11 +286,32 @@ for path in dir.iterdir():
                 if eef_pos[0] < bottle.get_pos()[0]:
                     reward = 1.
                     total_reward += reward
+                    # write out the configs for the episode
+                    # TODO: write out the episode
+                    # print(f"Writing out episode {path}")
+                    # results_dict = {
+                    #     'reached': cmd_idx
+                    # }
+                    # np.save(path, results_dict)
+
                     print(f"~~~~~Success at step {step}~~~~~~")
                     break
         step += 1
     if reward == 0:
         print(f"~~~~~Failed~~~~~~")
 print(f"Total reward: {total_reward} out of possible {trials_ran}. Success rate: {total_reward / trials_ran:1.2%}")
+results_dict = {
+    'total_reward': total_reward,
+    'trials_ran': trials_ran,
+    'success_rate': total_reward / trials_ran,
+    'args': vars(args),
+}
+
+# turn args into a string for the save filename
+args_str = '_'.join([f"{k}_{v}" for k, v in vars(args).items()])
+
+import json
+with open(f'results_{int(total_reward*100/trials_ran)}__{args_str}.json', 'w') as f:
+    json.dump(results_dict, f, indent=4)
         # time.sleep(0.2)
         # from IPython import embed; embed(); exit()
--- parameter_search.sh
+++ parameter_search.sh
@@ -1,8 +1,8 @@
 # call example.py with different radius and friction values
 
 for friction in 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0; do
-    for radius in 0.025 0.0275 0.03 0.0325 0.035 0.0375 0.04; do
+    for radius in 0.0275 0.03 0.0325 0.035; do
         echo "Running with radius $radius and friction $friction"
-        python example.py -r $radius -f $friction -d -v
+        python example.py -r $radius -f $friction
     done
 done
